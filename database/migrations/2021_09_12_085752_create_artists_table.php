7d3956ff9fefa3732f20df75ac925181fe80ba9f
Pofkinis <povkab@gmail.com>
2021-09-12 12:05:48 +0300

    Add softdeletes for artists

--- database/migrations/2021_09_12_085752_create_artists_table.php
+++ database/migrations/2021_09_12_085752_create_artists_table.php
@@ -18,6 +18,7 @@ class CreateArtistsTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->date('birth_date');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
