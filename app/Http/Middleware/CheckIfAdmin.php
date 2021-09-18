ce12ed53240217cdd4411747c29b6fad66da8ccc
Pofkinis <povkab@gmail.com>
2021-09-19 01:03:47 +0300

    Create middleware to check if admin

---
+++ app/Http/Middleware/CheckIfAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckIfAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(!$request->user()->is_admin){
+            return response()->json('Unauthorized', 401);
+        }
+        return $next($request);
+    }
+}
