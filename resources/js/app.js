3885092433416135af8c65a6bb1e71e222e8186c
Pofkinis <povkab@gmail.com>
2021-10-17 17:38:09 +0300

    Setup vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import Hello from './components/hello.vue';
+const vue = new Vue({
+    el: '#app',
+
+    components:{
+        'hello': Hello,
+    }
+});
