84a8e6bf54f91050dc7c769555d2f10dc8acbe30
Pofkinis <povkab@gmail.com>
2021-12-09 23:42:28 +0200

    Update seeders

--- database/seeders/AlbumsSeeder.php
+++ database/seeders/AlbumsSeeder.php
@@ -18,16 +18,19 @@ class AlbumsSeeder extends Seeder
             0 => [
                 'name' => 'Divide',
                 'artist_id' => 1,
+                'image_link' => 'https://m.media-amazon.com/images/I/61MHPCXEdFS._SX355_.jpg',
                 'release_date' => '2016-05-05'
             ],
             1 => [
                 'name' => 'Equals',
                 'artist_id' => 1,
+                'image_link' => 'https://upload.wikimedia.org/wikipedia/en/c/cd/Ed_Sheeran_-_Equals.png',
                 'release_date' => '2018-07-16'
             ],
             2 => [
                 'name' => 'Happier Than Ever',
                 'artist_id' => 2,
+                'image_link' => 'https://www.udiscovermusic.com/wp-content/uploads/2021/05/Billie-Eilish-Happier-Than-Ever-Tour.jpg',
                 'release_date' => '2019-01-20'
             ],
         ];
--- database/seeders/SongsSeeder.php
+++ database/seeders/SongsSeeder.php
@@ -18,49 +18,49 @@ class SongsSeeder extends Seeder
             0 => [
                 'name' => 'Shape of You',
                 'album_id' => 1,
-                'link_to_song' => 'https://www.youtube.com/watch?v=JGwWNGJdvx8&ab_channel=EdSheeran',
+                'link_to_song' => 'https://www.youtube.com/embed/JGwWNGJdvx8',
                 'type' => 'pop'
             ],
             1 => [
                 'name' => 'Perfect',
                 'album_id' => 1,
-                'link_to_song' => 'https://www.youtube.com/watch?v=2Vv-BfVoq4g&ab_channel=EdSheeran',
+                'link_to_song' => 'https://www.youtube.com/embed/2Vv-BfVoq4g',
                 'type' => 'hip hop'
             ],
             2 => [
                 'name' => 'Galway girl',
                 'album_id' => 1,
-                'link_to_song' => 'https://www.youtube.com/watch?v=87gWaABqGYs&ab_channel=EdSheeran',
+                'link_to_song' => 'https://www.youtube.com/embed/87gWaABqGYs',
                 'type' => 'pop'
             ],
             3 => [
                 'name' => 'First time',
                 'album_id' => 2,
-                'link_to_song' => 'https://www.youtube.com/watch?v=ZcrP5Oqsx1U&ab_channel=LukeGittins',
+                'link_to_song' => 'https://www.youtube.com/embed/ZcrP5Oqsx1U',
                 'type' => 'hip hop'
             ],
             4 => [
                 'name' => 'One',
                 'album_id' => 2,
-                'link_to_song' => 'https://www.youtube.com/watch?v=afusUyiYOwk&ab_channel=DopeLyrics',
+                'link_to_song' => 'https://www.youtube.com/embed/afusUyiYOwk',
                 'type' => 'pop'
             ],
             5 => [
                 'name' => 'Lego house',
                 'album_id' => 2,
-                'link_to_song' => 'https://www.youtube.com/watch?v=c4BLVznuWnU&ab_channel=EdSheeran',
+                'link_to_song' => 'https://www.youtube.com/embed/c4BLVznuWnU',
                 'type' => 'pop'
             ],
             6 => [
                 'name' => 'Happier Than Ever"',
                 'album_id' => 3,
-                'link_to_song' => 'https://www.youtube.com/watch?v=5GJWxDKyk3A&ab_channel=BillieEilishVEVO',
+                'link_to_song' => 'https://www.youtube.com/embed/5GJWxDKyk3A',
                 'type' => 'electropop'
             ],
             7 => [
                 'name' => 'Overheated',
                 'album_id' => 3,
-                'link_to_song' => 'https://www.youtube.com/watch?v=vg6V2UWSjiM&ab_channel=BillieEilishVEVO',
+                'link_to_song' => 'https://www.youtube.com/embed/vg6V2UWSjiM',
                 'type' => 'electropop'
             ],
         ];
