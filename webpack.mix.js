b88dce50f693924680a16b2beccb9491f09f03cd
Pofkinis <povkab@gmail.com>
2021-12-09 23:42:54 +0200

    add notyf lib

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,6 @@ mix.js('resources/js/app.js', 'public/js').vue()
     .postCss('resources/css/app.css', 'public/css', [
         require("tailwindcss"),
     ]);
+
+mix.copy('node_modules/notyf/notyf.min.js', 'public/assets/js/notyf.min.js');
+mix.copy('node_modules/notyf/notyf.min.css', 'public/assets/css/notyf.min.css');
